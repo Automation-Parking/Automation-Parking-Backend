dd730ee96e1f6abf66e8d3775e94cfbb23f9a9be
sans18-coder <iksanwijaya018@gmail.com>
2024-12-06 18:03:06 +0700

    update ademinwebService

---
+++ src/controller/adminWeb-controller.js
@@ -0,0 +1,99 @@
+import adminWebService from "../service/adminWeb-service.js";
+
+// Controller untuk menangani request GET /customers
+const getParkingOutData = async (req, res, next) => {
+  try {
+    const params = {
+      search: req.query.search || "",
+      bulan: req.query.bulan ? parseInt(req.query.bulan) : null,
+      tahun: req.query.tahun ? parseInt(req.query.tahun) : null,
+      wilayah: req.query.wilayah || "",
+      kotaProvinsi: req.query.kota_provinsi || "",
+      jenisKendaraan: req.query.jenisKendaraan || "",
+      page: req.query.page ? parseInt(req.query.page) : 1,
+      pageSize: req.query.pageSize ? parseInt(req.query.pageSize) : 10,
+    };
+
+    // Memanggil service untuk mendapatkan data
+    const { data, total } = await adminWebService.getParkingData(params);
+
+    // Mengirimkan respons
+    return res.status(200).json({
+      success: true,
+      message: "Success",
+      total,
+      data,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getParkingByCity = async (res, next) => {
+  try {
+    const data = await adminWebService.getParkingDataByCity();
+    return res.status(200).json({
+      success: true,
+      message: "Success",
+      data,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getParkingByRegion = async (res, next) => {
+  try {
+    const data = await adminWebService.getParkingDataByRegion();
+    return res.status(200).json({
+      success: true,
+      message: "Success",
+      data,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+const getParkingByMonth = async (req, res, next) => {
+  try {
+    const params = {
+      tahun: req.query.tahun
+        ? parseInt(req.query.tahun)
+        : new Date().getFullYear(),
+    };
+    const data = await adminWebService.getParkingDataByMonth(params);
+    return res.status(200).json({
+      success: true,
+      message: "Success",
+      data,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+const getParkingByWeekInMonth = async (req, res, next) => {
+  try {
+    const params = {
+      tahun: req.query.tahun
+        ? parseInt(req.query.tahun)
+        : new Date().getFullYear(),
+    };
+    const data = await adminWebService.getParkingDataByWeekInMonth(params);
+    return res.status(200).json({
+      success: true,
+      message: "Success",
+      data,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default {
+  getParkingOutData,
+  getParkingByCity,
+  getParkingByRegion,
+  getParkingByRegion,
+  getParkingByMonth,
+  getParkingByWeekInMonth,
+};
--- src/route/admin-api.js
+++ src/route/admin-api.js
@@ -1,10 +1,14 @@
 // src/route/api.js
 import express from "express";
-import parkingOutController from "../controller/parkingOut-controller.js";
+import adminWebController from "../controller/adminWeb-controller.js";
 import reportController from "../controller/report-controller.js";
 const adminRouter = new express.Router();
 
-adminRouter.get("/api/getParkingOut", parkingOutController.getParkingOutData);
+adminRouter.get("/api/getParkingOut", adminWebController.getParkingOutData);
+adminRouter.get("/api/getParkingByCity", adminWebController.getParkingByCity);
+adminRouter.get("/api/getParkingByRegion",adminWebController.getParkingByRegion);
+adminRouter.post("/api/getParkingByMonth",adminWebController.getParkingByMonth);
+adminRouter.post("/api/getParkingByWeek",adminWebController.getParkingByWeekInMonth);
 adminRouter.all("/api/report", reportController.generateReport);
 
 export { adminRouter };
